98d70ea92b601ac2dc8c0c585fb910147fa9ada5
Alexys <alexyslc@gmail.com>
2021-06-21 18:56:47 -0500

    Init routing class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import Todo from "./components/Todo";
-
 function App() {
   return (
     <div>
-      <h1>My todos</h1>
-      <Todo title="learn react" />
-      <Todo title="take course youtube" />
     </div>
   );
 }
