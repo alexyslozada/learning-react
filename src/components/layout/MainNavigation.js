94958908ee81a3d36cdff420d9d10e4bfea38e08
Alexys <alexyslc@gmail.com>
2021-06-21 19:55:23 -0500

    Second class from course

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css"
+
+function MainNavigation() {
+  return (
+  <header className={classes.header}>
+    <div className={classes.logo}>Alexys Meetups</div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/new-meetup">New meetup</Link>
+        </li>
+        <li>
+          <Link to="/favorites">Favorites</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  )
+}
+
+export default MainNavigation;
